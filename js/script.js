50abcd5a1b6bf40519570e4016b79ae47f201da2
Joseph Bertino <jbertin@sandia.gov>
2020-11-26 18:53:03 -0700

    trying for exceeds expectations

--- js/script.js
+++ js/script.js
@@ -12,33 +12,90 @@ const searchHTML = `
       <input id="search" placeholder="Search by name...">
       <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
     </label>
-`
+`;
 header.insertAdjacentHTML('beforeend',searchHTML);
 
-// Add functionality to the Search Bar
-
-// const searchFN = (searchInput, names) => {
-//   console.log(searchInput);
-//   console.log(names);
-//   [...names].forEach((name) => {
-//     name.classList.remove('match'); 
-//     if (searchInput.value.length !== 0 && name.textContent.toLowerCase().includes(searchInput.value.toLowerCase())) {
-//       name.classList.add('match');
-//     }
-//   });
-// };
-
+//////// Add functionality to the Search Bar
+const searchLabel = document.querySelector('label.student-search');
+const searchInput = searchLabel.querySelector('input');
+const searchButton = searchLabel.querySelector('button');
 
 /**
- * Create and insert/append the elements needed to display a "page" of nine students.
+ *
+ * When the "Search" button is clicked, call showSubPage().
+ * 
+ */
+searchButton.addEventListener('click', (e) => {
+    showSubPage(e);
+});
+
+/**
+ *
+ * When the "Search" input value is updated, call showSubPage().
+ *
+ */
+searchInput.addEventListener('keyup', (e) => {
+    showSubPage(e);
+});
+
+/**
+ *
+ * Filter and display the student cards based on the search pattern.
+ *
+ * @param {Event}   e   The search event. Not used currently.
+ * 
+ */
+const showSubPage = (e) => {
+    /*
+        trim() whitespace to allow for the possibility of a pattern including portions of both the first and last name. 
+        The pattern "ary j" will yield a match for "Mary Jane", while the pattern "ary " will match "Mary Jane" as well as "Karyn Fields".
+    */
+    var pattern = searchInput.value.toLowerCase().trim();
+    var sublist = [];
+    
+    // Iterate through the complete list of students, looking for those whose names match or include the search pattern entered by the user
+    data.forEach((student) => {
+        var fullName = student.name.first + ' ' + student.name.last;
+        // If the pattern is found in the student name, add the student to our filtered list
+        if (fullName.toLowerCase().includes(pattern)) {
+            sublist.push(student);
+        }
+    });
+    // Display the filtered student cards onto the page and update the pagination buttons.
+    if (sublist.length > 0) {
+        showPage(sublist, 1); 
+        addPagination(sublist);
+    }
+    // If the filtered student list is empty, display a "No Results" message on screen
+    else {
+        noResults();
+    }   
+};
+
+/**
+ * Print a "No results" message to the screen.
+ * 
+ * The ul.innerHTML will be re-set when the search pattern is updated.
+ * 
+*/
+const noResults = () => {
+    const ul = document.querySelector('ul.student-list');
+    ul.innerHTML = `
+        <p>No results found. Try another search pattern.</p>
+    `;
+};
+
+////// END SEARCH BAR FUNCTIONALITY
+
+/**
+ * Insert/append the elements needed to display a page of nine students.
  *
  * Given a list of students (with biographical details) and a page number, generate a page
  * displaying nine of those students.
  *
- * @param {Array}   list    The full list of students
+ * @param {Array}   list    The full list of student data.
  * @param {Number}  page    The page to generate. Index starts at 1. 
  *
- * @return {null}   Does not return anything
 */
 const showPage = (list, page) => {
     // Create two variables to store the start index and the end index of the list items to be displayed on the given page.
@@ -46,14 +103,16 @@ const showPage = (list, page) => {
     const endIdx = (page * 9);
 
     // Select the UL element with a class of student-list and assign its value to a variable.
-    const ul = document.querySelector('.student-list');
+    const ul = document.querySelector('ul.student-list');
     
     // Use the innerHTML property set the HTML content of the student-list variable you just created to an empty string. This will remove any students that might have previously been displayed.
     ul.innerHTML = '';
     
     // Loop over the list parameter.
     for (let i = 0; i < list.length; i++) {
+        // If the current index (i) is greater than or equal to the start index variable and less than the end index variable...
         if (startIdx <= i && i < endIdx) {
+            // Create the DOM elements needed to display the information for each matching student as you iterate over the list parameter. 
             var info = list[i]
             var liHTML = `
                 <li class="student-item cf">
@@ -67,7 +126,7 @@ const showPage = (list, page) => {
                   </div>
                 </li>
             `;
-            
+            // Insert the elements you have created to the student-list variable you created earlier.
             ul.insertAdjacentHTML('beforeend', liHTML);
         }
     }
@@ -75,29 +134,32 @@ const showPage = (list, page) => {
 
 
 /**
- * This function will create and insert/append the elements needed for the pagination buttons.
+ * Create and insert/append the elements needed for the pagination buttons.
  *
  * @param {Array}   list    Array of the student data.
  *
- * @return {null}   Does not return anything
 */
 const addPagination = (list) => {
     // Create a variable to store the value of the number of pagination buttons needed.
     const pageCount = Math.ceil(list.length / 9)
     
-    // Select the UL element with a class of link-list and assign its value to a variable. Use the innerHTML property set the HTML content of the link-list variable you just created to an empty string. 
-    const buttonList = document.querySelector('.link-list');
+    // Select the UL element with a class of link-list and assign its value to a variable. 
+    const buttonList = document.querySelector('ul.link-list');
+    // Use the innerHTML property set the HTML content of the link-list variable you just created to an empty string. 
     buttonList.innerHTML = '';
     
     // Loop over the variable for the number of pages needed that you created earlier.
-    for (let i = 1; i <= pageCount; i++) {
-        // Create the DOM elements needed to display the pagination button as you iterate over the number of pages.
-        var buttonHTML = `
-            <li>
-              <button type="button">${i}</button>
-            </li>
-        `;
-        buttonList.insertAdjacentHTML('beforeend',buttonHTML);
+    // Only create pagination buttons if there will be at least 2.
+    if (pageCount > 1) {    
+        for (let i = 1; i <= pageCount; i++) {
+            // Create the DOM elements needed to display the pagination button as you iterate over the number of pages.
+            var buttonHTML = `
+                <li>
+                  <button type="button">${i}</button>
+                </li>
+            `;
+            buttonList.insertAdjacentHTML('beforeend',buttonHTML);
+        }
     }
     
     // Select the first pagination button and give it a class name of active.
@@ -108,13 +170,10 @@ const addPagination = (list) => {
     buttonList.addEventListener('click', (e) => {
         // The click event should only fire when the buttons are clicked.
         if (e.target.tagName === 'BUTTON') {
-
             // Remove the active class from any other pagination button.
-            [...buttonList.children].forEach((li) => {
-                var b = li.querySelector('button');
-                if (b !== e.target) {
-                    b.className = '';
-                }
+            const buttons = buttonList.querySelectorAll('BUTTON');
+            [...buttons].forEach((b) => {
+                b.className = '';
             });
             
             // Add the active class to the pagination button that was just clicked.
@@ -122,7 +181,7 @@ const addPagination = (list) => {
             
             // Call the showPage function passing the list parameter and the page number to display as arguments.
             const page = parseInt(e.target.textContent, 10);
-            showPage(data, page);
+            showPage(list, page);
         }
     });
 };
